e244438db700395e57654aec12e01c9c885394d5
alvarezb <alvarezb@users.noreply.github.com>
2025-05-18 17:13:41 -0700

    test setup

--- components/TLC59116/TLC59116.cpp
+++ components/TLC59116/TLC59116.cpp
@@ -64,7 +64,7 @@ void TLC59116Output::begin() {
         writeRegister(TLC59116_LEDOUT1, 0b10101010);
         writeRegister(TLC59116_LEDOUT2, 0b10101010);
         writeRegister(TLC59116_LEDOUT3, 0b10101010);
-        analogWrite(0, 0);
+        analogWrite(0, 255);
         analogWrite(1, 0);
         analogWrite(2, 0);
         analogWrite(3, 0);
@@ -72,14 +72,14 @@ void TLC59116Output::begin() {
         analogWrite(5, 0);
         analogWrite(6, 0);
         analogWrite(7, 0);
-        analogWrite(8, 0);
+        analogWrite(8, 255);
         analogWrite(9, 0);
         analogWrite(10, 0);
         analogWrite(11, 0);
         analogWrite(12, 0);
         analogWrite(13, 0);
         analogWrite(14, 0);
-        analogWrite(15, 0);
+        analogWrite(15, 255);
     }
     begun_ = 1;
 }
@@ -94,6 +94,7 @@ void TLC59116Output::writeRegister(uint8_t reg, uint8_t val) {
 
 void TLC59116Output::analogWrite(uint8_t chan, uint8_t b) {
     writeRegister(TLC59116_PWM0 + (chan & 0x0F), b);
+
 }
 
 /* ESP home methods */
@@ -110,6 +111,7 @@ void TLC59116Output::dump_config() {
     ESP_LOGCONFIG(TAG, " addr = %i", addr_);
 }
 light::LightTraits TLC59116Output::get_traits() {
+  ESP_LOGD(TAG, "get_traits")
   auto traits = light::LightTraits();
   traits.set_supported_color_modes({light::ColorMode::BRIGHTNESS});
   return traits;
